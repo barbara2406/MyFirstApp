f78965308c936190189031d881442438c40706ff
Barbara Muñoz <bmunoz@nisum.com>
2018-02-14 17:35:00 -0300

    First commit

---
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.administrator.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    int cont =0;
+    TextView text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Button less = (Button) findViewById(R.id.less);
+        Button more = (Button) findViewById(R.id.more);
+        text = (TextView) findViewById(R.id.cont);
+
+        more.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                cont++;
+                text.setText(Integer.toString(cont));
+            }
+        });
+
+        less.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cont--;
+                text.setText(Integer.toString(cont));
+            }
+        });
+    }
+}
